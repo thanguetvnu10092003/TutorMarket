c3b909db5e346898e8e0d06cbb54879350e6f027
LeToanThang <toanthangvietduc@gmail.com>
2026-03-21 14:21:33 +0700

    feat: add mock payment system and clean up workspace

--- scripts/check-prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log('Prisma Models:', Object.keys(prisma).filter(k => !k.startsWith('_') && !k.startsWith('$')));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
